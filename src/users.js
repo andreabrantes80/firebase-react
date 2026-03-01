a8f47a03968c78ba7212b83be3ca718895ba51c8
andreabrantes80 <meloabrantes@gmail.com>
2026-02-28 22:28:13 -0300

    deletando um item do banco

--- src/users.js
+++ src/users.js
@@ -1,11 +1,28 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import { db } from './firebaseConnection';
+import { doc, deleteDoc } from 'firebase/firestore';
 
 export function UsersList({ data }) {
+
+
+    async function handleDeleteItem() {
+        // console.log('Excluir item: ' + data.id);
+        const docRef = doc(db, "users", data.id);
+        await deleteDoc(docRef);
+
+    }
+
+
     return (
         <View style={styles.container}>
             <Text>Nome: {data?.nome}</Text>
             <Text>Idade: {data?.idade}</Text>
             <Text>Cargo: {data?.cargo}</Text>
+
+            <TouchableOpacity style={styles.button}  onPress={handleDeleteItem}>
+                <Text style={styles.buttonText}>Excluir</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -20,5 +37,16 @@ const styles = StyleSheet.create({
     item: {
         color: "#000",
         fontSize: 16,
+    },
+    button: {
+        backgroundColor: "#ff0000",
+        alignSelf: "flex-start",
+        padding: 6,
+        borderRadius: 4,
+        marginTop: 12,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 12,
     }
 })
